ecb094dc1bbd40763964a52503544270dca529cd
lazlo <user@inginerus-MacBook-Air.local>
2024-08-08 07:40:41 +0300

    rewrote tcpstream usage to be threadsafe, using struct for 2-way tcp communication between client-server

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,13 +1,8 @@
-use lazy_static::lazy_static;
 use rand::Rng;
 use std::io::{stdin, Read, Write};
 use std::net::TcpStream;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use std::thread;
-
-lazy_static! {
-    static ref NICKNAMES: Mutex<Vec<String>> = Mutex::new(Vec::new());
-}
 
 fn random_number() -> String {
     let mut rng = rand::thread_rng();
@@ -15,70 +10,102 @@ fn random_number() -> String {
     num.to_string()
 }
 
-fn join_chat(mut stream: TcpStream) {
+struct SharedStream {
+    read_stream: Arc<Mutex<TcpStream>>,
+    write_stream: Arc<Mutex<TcpStream>>,
+}
+
+impl SharedStream {
+    fn new(stream: TcpStream) -> Self {
+        let read_stream = Arc::new(Mutex::new(
+            stream.try_clone().expect("Failed to clone stream"),
+        ));
+        let write_stream = Arc::new(Mutex::new(stream));
+        SharedStream {
+            read_stream,
+            write_stream,
+        }
+    }
+}
+
+fn join_chat(shared_stream: Arc<SharedStream>) {
     let mut buffer = [0; 1024];
     let mut did_try = false;
     let mut nickname = String::new();
 
     loop {
-        match stream.read(&mut buffer) {
-            Ok(bytes_read) => {
-                let response = String::from_utf8_lossy(&buffer[..bytes_read]);
-                println!("{}", response);
-                if bytes_read == 0 {
-                    println!("Connection closed by the server.");
-                    break;
-                }
+        let bytes_read;
+        {
+            let mut shared_read_lock = shared_stream
+                .read_stream
+                .lock()
+                .expect("Failed to lock read lock");
+            bytes_read = shared_read_lock
+                .read(&mut buffer)
+                .expect("Failed to read from stream");
+        }
 
-                if response.trim() == "NICK" {
-                    let mut current = String::new();
-                    if !did_try {
-                        println!("Choose a nickname: ");
-                    } else {
-                        println!("Nickname already taken, choose another: ");
-                    }
-                    did_try = true;
+        if bytes_read == 0 {
+            println!("Connection closed by the server.");
+            break;
+        }
 
-                    nickname.clear();
-                    stdin()
-                        .read_line(&mut current)
-                        .expect("Failed to read line");
-                    nickname = current.trim().to_string();
+        let response = String::from_utf8_lossy(&buffer[..bytes_read]);
 
-                    if nickname.is_empty() {
-                        let random_num = random_number();
-                        nickname = "Anonymous".to_string() + &random_num;
-                    }
-                    println!("nickname = {}", nickname);
-                    stream
-                        .write(nickname.as_bytes())
-                        .expect("Failed to write to stream");
-                }
-
-                if response.trim() == "OK" {
-                    println!("Connected as: {}", nickname);
-                    break;
-                }
-
-                if response.trim().is_empty() {
-                    if let Err(e) = stream.write_all(b"/exit\n") {
-                        eprintln!("Failed to send message: {}", e);
-                    }
-                    break;
-                }
+        if response.trim() == "NICK" {
+            let mut current = String::new();
+            if !did_try {
+                println!("Choose a nickname: ");
+            } else {
+                println!("Nickname already taken, choose another: ");
             }
-            Err(e) => {
-                eprintln!("Error: {}", e);
-                break;
+            did_try = true;
+
+            stdin()
+                .read_line(&mut current)
+                .expect("Failed to read line");
+            nickname = current.trim().to_string();
+
+            if nickname.is_empty() {
+                let random_num = random_number();
+                nickname = "Anonymous".to_string() + &random_num;
+            }
+
+            {
+                let mut shared_write_lock = shared_stream
+                    .write_stream
+                    .lock()
+                    .expect("Failed to lock write lock");
+                shared_write_lock
+                    .write_all(nickname.as_bytes())
+                    .expect("Failed to write to stream");
             }
         }
+
+        if response.trim() == "OK" {
+            println!("Connected as: {}", nickname);
+            break;
+        }
+
+        if response.trim().is_empty() {
+            let mut shared_write_lock = shared_stream
+                .write_stream
+                .lock()
+                .expect("Failed to lock write stream");
+            if let Err(e) = shared_write_lock.write_all(b"/exit\n") {
+                eprintln!("Failed to send message: {}", e);
+            }
+            break;
+        }
     }
-    write_chat(stream);
+    write_chat(shared_stream);
 }
 
-fn write_chat(mut stream: TcpStream) {
-    thread::spawn(|| {
-        read_chat(stream);
+fn write_chat(shared_stream: Arc<SharedStream>) {
+    // Spawn a new thread for reading chat
+    let stream_clone = Arc::clone(&shared_stream);
+    thread::spawn(move || {
+        read_chat(stream_clone);
     });
 
     loop {
@@ -86,30 +113,44 @@ fn write_chat(mut stream: TcpStream) {
         stdin()
             .read_line(&mut message)
             .expect("Failed to read line");
+        let mut stream_lock = shared_stream
+            .write_stream
+            .lock()
+            .expect("Failed to lock write stream");
+        if let Err(e) = stream_lock.write_all(message.as_bytes()) {
+            eprintln!("Failed to send message: {}", e);
+            break;
+        }
     }
 }
 
-fn read_chat(mut stream: TcpStream) {
+fn read_chat(shared_stream: Arc<SharedStream>) {
     let mut buffer = [0; 1024];
     loop {
-        match stream.read(&mut buffer) {
-            Ok(bytes_read) => {
-                let response = String::from_utf8_lossy(&buffer[..bytes_read]);
-                println!("{}", response);
-                if bytes_read == 0 {
-                    println!("Connection closed by the server.");
-                    break;
-                }
-            }
-            Err(e) => {
-                eprintln!("Error: {}", e);
-                break;
-            }
+        let bytes_read;
+        {
+            let mut stream_lock = shared_stream
+                .read_stream
+                .lock()
+                .expect("Failed to lock read stream");
+            bytes_read = stream_lock
+                .read(&mut buffer)
+                .expect("Failed to read from stream");
         }
+
+        if bytes_read == 0 {
+            println!("Connection closed by the server.");
+            break;
+        }
+
+        let response = String::from_utf8_lossy(&buffer);
+        print!("{}", response);
     }
 }
 
 fn main() {
     let stream = TcpStream::connect("127.0.0.1:44454").expect("Failed to connect to server");
-    join_chat(stream);
+    let shared_stream = Arc::new(SharedStream::new(stream));
+
+    join_chat(shared_stream);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,139 @@
 use lazy_static::lazy_static;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 lazy_static! {
-    static ref NICKNAMES: Mutex<Vec<String>> = Mutex::new(Vec::new());
-    static ref STREAMS: Mutex<Vec<TcpStream>> = Mutex::new(Vec::new());
+    static ref STREAMS: Mutex<Vec<Arc<SharedStream>>> = Mutex::new(Vec::new());
 }
 
-fn join_client(mut stream: TcpStream) {
-    let mut buf = [0; 1024];
-    let mut message = "NICK";
-    loop {
-        stream
-            .write_all(message.as_bytes())
-            .expect("Failed to write to stream");
+struct SharedStream {
+    read_stream: Arc<Mutex<TcpStream>>,
+    write_stream: Arc<Mutex<TcpStream>>,
+    nickname: Mutex<String>,
+}
 
-        stream.read(&mut buf).expect("Failed to read from stream");
-        let mut response = String::from_utf8_lossy(&buf).trim().to_string();
+impl SharedStream {
+    fn new(stream: TcpStream) -> Self {
+        let read_stream = Arc::new(Mutex::new(
+            stream.try_clone().expect("Failed to clone stream"),
+        ));
+        let write_stream = Arc::new(Mutex::new(stream));
 
-        let mut nicknames = NICKNAMES.lock().expect("Failed to lock mutex");
-
-        if !nicknames.contains(&response) {
-            println!("{} joined the chat", response);
-            message = "OK";
-            stream
-                .write_all(message.as_bytes())
-                .expect("Failed to write to stream");
-            nicknames.push(response.clone());
-            let mut streams = STREAMS.lock().expect("Failed to lock mutex");
-            streams.push(stream.try_clone().expect("Failed to clone stream"));
-            handle_client_messages(stream);
-            break;
-        } else {
-            message = "NICK";
-            response.clear();
+        SharedStream {
+            read_stream,
+            write_stream,
+            nickname: Mutex::new(String::new()),
         }
     }
 }
 
-fn handle_client_messages(mut stream: TcpStream) {
-    let mut buf = [0; 1024];
-    println!("hey");
+fn check_if_nickname_exists(nickname: String) -> bool {
+    let streams = STREAMS.lock().expect("Failed to lock mutex");
+    println!("streams currently: {}", streams.len());
+    for stream in streams.iter() {
+        let stream_nickname = stream.nickname.lock().expect("Error locking nickname");
+        if *stream_nickname == nickname {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn join_client(shared_stream: Arc<SharedStream>) {
+    let mut buffer = [0; 1024];
+    let mut message = "NICK";
+    let mut client_read_lock = shared_stream
+        .read_stream
+        .lock()
+        .expect("Failed to lock stream");
+    let mut client_write_lock = shared_stream
+        .write_stream
+        .lock()
+        .expect("Failed to lock stream");
     loop {
-        stream.read(&mut buf).expect("Failed to read from stream");
-        let response = String::from_utf8_lossy(&buf).trim().to_string();
-        println!("{}", response);
-        if response == "exit" {
+        client_read_lock
+            .write_all(message.as_bytes())
+            .expect("Failed to write to stream");
+        client_write_lock
+            .read(&mut buffer)
+            .expect("Failed to read from stream");
+        let nickname = String::from_utf8_lossy(&buffer).trim().to_string();
+        if !check_if_nickname_exists(nickname.clone()) {
+            println!("im stuck!");
+            let stream_clone = Arc::clone(&shared_stream);
+            {
+                let mut stream_nickname_lock = shared_stream
+                    .nickname
+                    .lock()
+                    .expect("Failed to lock nickname");
+                *stream_nickname_lock = nickname.clone();
+            }
+            println!("{} joined the chat", nickname);
+            message = "OK";
+            client_write_lock
+                .write_all(message.as_bytes())
+                .expect("Failed to write to stream");
+            let mut streams = STREAMS.lock().expect("Failed to lock mutex");
+            streams.push(shared_stream.clone());
+            println!("stream pushed");
+            thread::spawn(move || {
+                handle_client_messages(stream_clone);
+            });
             break;
+        } else {
+            message = "NICK";
+        }
+    }
+}
+
+fn handle_client_messages(shared_stream: Arc<SharedStream>) {
+    let mut buffer = [0; 1024];
+    let current_stream_ptr = Arc::as_ptr(&shared_stream) as *const Mutex<TcpStream>;
+    loop {
+        let mut stream_lock = shared_stream
+            .read_stream
+            .lock()
+            .expect("Failed to lock stream");
+        match stream_lock.read(&mut buffer) {
+            Ok(bytes_read) => {
+                println!("Bytes read: {}", bytes_read);
+                let mut response = String::from_utf8_lossy(&buffer).trim().to_string();
+                if bytes_read != 0 {
+                    {
+                        let nickname_guard = shared_stream
+                            .nickname
+                            .lock()
+                            .expect("Failed to lock nickname");
+                        let nickname = &*nickname_guard;
+                        response = format!("<{}>: {}", nickname, response);
+                    }
+                    let streams = STREAMS.lock().expect("Failed to lock mutex");
+                    for other_stream in streams.iter() {
+                        let other_stream_ptr = Arc::as_ptr(other_stream) as *const Mutex<TcpStream>;
+                        if other_stream_ptr != current_stream_ptr {
+                            let mut other_write_lock = other_stream
+                                .write_stream
+                                .lock()
+                                .expect("Failed to lock write stream");
+                            other_write_lock
+                                .write_all(response.as_bytes())
+                                .expect("Failed to write to stream");
+                        }
+                    }
+                } else {
+                    println!("Connection closed by the client.");
+                    break;
+                }
+                if response == "exit" {
+                    break;
+                }
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                break;
+            }
         }
     }
 }
@@ -60,7 +145,7 @@ fn main() {
         match stream {
             Ok(stream) => {
                 thread::spawn(|| {
-                    join_client(stream);
+                    join_client(Arc::new(SharedStream::new(stream)));
                 });
             }
             Err(e) => {
